0a86c6f9816897220bd702c38cd6ca66bc97825b
jtp5 <league@WTS22.attlocal.net>
2018-04-22 14:26:31 -0700

    4/22/18

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,66 @@
 package threads;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
 
+	public static void drawCircle(Robot rob, int r) {
+		for (int i = 0; i < 360; i++) {
+			rob.penDown();
+			rob.turn(1);
+			rob.move(r);
+		}
+	}
+
+	public static void main(String[] args) {
+		Robot r1 = new Robot();
+		Robot r2 = new Robot();
+		Robot r3 = new Robot();
+		Robot r4 = new Robot();
+		Robot r5 = new Robot();
+		Thread t1 = new Thread(() -> {
+			r1.setSpeed(1000);
+			r1.setPenColor(Color.BLUE);
+			r1.moveTo(500, 300);
+			drawCircle(r1, 1);
+			r1.hide();
+		});
+		Thread t2 = new Thread(() -> {
+			r2.setSpeed(1000);
+			r2.setPenColor(Color.BLACK);
+			r2.moveTo(625, 300);
+			drawCircle(r2, 1);
+			r2.hide();
+		});
+		Thread t3 = new Thread(() -> {
+			r3.setSpeed(1000);
+			r3.setPenColor(Color.RED);
+			r3.moveTo(750, 300);
+			drawCircle(r3, 1);
+			r3.hide();
+		});
+		Thread t4 = new Thread(() -> {
+			r4.setSpeed(1000);
+			r4.setPenColor(Color.YELLOW);
+			r4.moveTo(563, 350);
+			drawCircle(r4, 1);
+			r4.hide();
+		});
+		Thread t5 = new Thread(() -> {
+			r5.setSpeed(1000);
+			r5.setPenColor(Color.GREEN);
+			r5.moveTo(688, 350);
+			drawCircle(r5, 1);
+			r5.hide();
+		});
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+	}
 }
-
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,32 @@
+package threads;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+	public static void program(int count) {
+
+		int num = count;
+
+		Thread t1 = new Thread(() -> {
+			if (count <= 50) {
+				program(count + 1);
+				System.out.println("Hello from Thread " + num);
+			}
+		});
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		program(1);
+	}
 }
